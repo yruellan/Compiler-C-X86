c6c89cd06662f319d1576241006252f1c7cce6ac
Yann <ruellan.yann@laposte.net>
2024-11-29 12:59:59 +0100

    Refactor compile-gcc.sh to use temporary file naming and include stdbool.h

--- compile-gcc.sh
+++ compile-gcc.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-touch "_temp.c"
-echo "#include <stdlib.h>" > "_temp.c"
-echo "#include <stdio.h>" >> "_temp.c"
-echo "void print_int(int x){ printf(\"%d\n\",x);}" >> "_temp.c"
-echo "void print_char(char x){ printf(\"%c\n\",x);}" >> "_temp.c"
-cat $1 >> "_temp.c"
-gcc -S -O3 "_temp.c" -o file.s
+c_file="${1:-file.c}"
+temp_file="${c_file%".c"}__tmp.c"
+s_file="${c_file%".c"}.s"
+
+echo "#include <stdlib.h>" > $temp_file
+echo "#include <stdio.h>" >> $temp_file
+echo "#include <stdbool.h>" >> $temp_file
+echo "void print_int(int x){ printf(\"%d\n\",x);}" >> $temp_file
+echo "void print_char(char x){ printf(\"%c\n\",x);}" >> $temp_file
+echo "" >> $temp_file
+cat $c_file >> $temp_file
+gcc -S -O3 $temp_file -o $s_file
+rm $temp_file
